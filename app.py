ea295585b33ad6c308af0b214799cc8d389ec13a
Vishwa Bhushan Dewangan <53945080+vishwa30@users.noreply.github.com>
2020-10-23 15:50:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,12 @@ def predict_route():
         basepath, 'uploads', secure_filename(f.filename))
     f.save(file_path)
     print(file_path)
-    # b = r'C:\Users\vishw\PycharmProjects\flask_learning\uploads\0ba3d536-8732-4ea1-b3e1-a1be86e5dc6a___RS_Erly.B_9499.JPG'
     apparel=getPrediction(file_path)
     print(apparel)
     return(apparel)
 
 
-# def decodeImage(imgstring, fileName):
-#     imgdata = base64.b64decode(imgstring)
-#     with open(fileName, 'wb') as f:
-#         f.write(imgdata)
-#         f.close()
-#
-
 
 
 if __name__=='__main__':
     app.run(debug=True)
-    print('something')
