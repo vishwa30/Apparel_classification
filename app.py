53e41af1a63040c506e6270ba8e2afc643af4ce9
Vishwa Bhushan Dewangan <vishwabhushan30@gmail.com>
2020-10-23 16:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from apparel_detection import getPrediction
+#from apparel_detection import get_dict
 import base64
 from werkzeug.utils import secure_filename
 import sys
@@ -7,7 +7,6 @@ import os
 ##In krish cotton static folder is also needed.
 
 app=Flask(__name__)
-
 
 @app.route('/')
 def func():
@@ -23,22 +22,19 @@ def predict_route():
     file_path = os.path.join(
         basepath, 'uploads', secure_filename(f.filename))
     f.save(file_path)
-    print(file_path)
-    # b = r'C:\Users\vishw\PycharmProjects\flask_learning\uploads\0ba3d536-8732-4ea1-b3e1-a1be86e5dc6a___RS_Erly.B_9499.JPG'
-    apparel=getPrediction(file_path)
-    print(apparel)
-    return(apparel)
+    return('Print good morning')
 
 
-# def decodeImage(imgstring, fileName):
-#     imgdata = base64.b64decode(imgstring)
-#     with open(fileName, 'wb') as f:
-#         f.write(imgdata)
-#         f.close()
-#
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
 
 
 
 if __name__=='__main__':
     app.run(debug=True)
-    print('something')
+
+
